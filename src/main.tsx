3a3e7447b26a95ee218c6faa65a95b5514aa2bb8
jparouca <joaopearoucaa@gmail.com>
2022-07-08 02:23:19 -0300

    feat: axios and miragejs implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createServer } from 'miragejs'
 import { App } from './App'
+
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 250,
+          type: 'deposit',
+          tag: 'PC',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
